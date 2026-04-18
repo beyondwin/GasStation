6587f0d31df5b6c009d58339bb5bb775d533679b
kws <coreanim@gmail.com>
2026-04-18 18:23:37 +0900

    fix: stack settings rows vertically

--- feature/settings/src/main/kotlin/com/gasstation/feature/settings/SettingsDetailScreen.kt
+++ feature/settings/src/main/kotlin/com/gasstation/feature/settings/SettingsDetailScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.PaddingValues
@@ -75,14 +76,16 @@ fun SettingsDetailScreen(
                             modifier = Modifier.fillMaxWidth(),
                             color = ColorWhite,
                         ) {
-                            options.forEachIndexed { index, option ->
-                                SettingsDetailRow(
-                                    label = option.label,
-                                    isSelected = option.isSelected,
-                                    onClick = { onOptionClick(option) },
-                                )
-                                if (index != options.lastIndex) {
-                                    HorizontalDivider(color = ColorGray4)
+                            Column {
+                                options.forEachIndexed { index, option ->
+                                    SettingsDetailRow(
+                                        label = option.label,
+                                        isSelected = option.isSelected,
+                                        onClick = { onOptionClick(option) },
+                                    )
+                                    if (index != options.lastIndex) {
+                                        HorizontalDivider(color = ColorGray4)
+                                    }
                                 }
                             }
                         }
