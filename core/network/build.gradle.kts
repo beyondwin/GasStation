78afb73c7689f1c37aa602f05630804daffd2420
kws <coreanim@gmail.com>
2026-05-05 19:23:59 +0900

    chore: catalog proj4j dependency

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -6,7 +6,7 @@ dependencies {
     implementation(project(":core:model"))
     implementation(libs.retrofit)
     implementation(libs.converter.gson)
-    implementation("org.locationtech.proj4j:proj4j:1.4.1")
+    implementation(libs.proj4j)
     testImplementation(libs.kotlinx.coroutines.core)
     testImplementation(libs.junit)
     testImplementation(libs.mockwebserver)
